6192ee0364930e538debe15e490d634bfe21f6a3
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-03-13 23:36:38 +0100

    add inserting element in heap

---
+++ inserting_in_heap.py
@@ -0,0 +1,16 @@
+def heapify(A, n, i):
+    parent = (i - 1)//2
+    if parent >= 0 and A[i] > A[parent]:
+        A[i], A[parent] = A[parent], A[i]
+        heapify(A, n, parent)
+
+
+
+def insert(A, elem):
+    A.append(elem)
+    heapify(A, len(A), len(A) - 1)
+
+tab = [5, 4, 2]
+
+insert(tab, 10)
+print(tab)
