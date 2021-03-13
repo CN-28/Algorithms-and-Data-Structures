de8af99165402f1ccd766c3f02fac8bb6bd1d3c4
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-03-13 13:39:24 +0100

    add quicksort

---
+++ quicksort.py
@@ -0,0 +1,19 @@
+def quicksort(A, p, r):
+    if p < r:
+        q = partition(A, p, r)
+        quicksort(A, p, q - 1)
+        quicksort(A, q + 1, r)
+    
+def partition(A, p, r):
+    pivot = A[r]
+    i = p - 1
+    for j in range(p, r):
+        if A[j] <= pivot:
+            i += 1
+            A[i], A[j] = A[j], A[i]
+    A[i + 1], A[r] = A[r], A[i + 1]
+    return i + 1
+
+tab = [4, 1, 5, 3, 4, 5]
+quicksort(tab, 0 , len(tab) - 1)
+print(tab)
