ad1757c286a0a4d2082317d631057333bfa3c6a6
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-03-23 20:30:34 +0100

    add counting sort

--- counting_sort.py
+++ counting_sort.py
@@ -1,20 +1,22 @@
 from random import randint
 n = 10
-Arr = [randint(0, n**2 - 1) for _ in range(n)]
+Arr = [randint(0, 55) for _ in range(n)]
 
-count = [0]*(n**2)
-for i in range(len(Arr)):
-    count[Arr[i]] += 1
-
-
-for i in range(n**2):
-    count[i] += count[i - 1]
+def counting_sort(Arr, k):
+    count = [0]*k
+    for i in range(len(Arr)):
+        count[Arr[i]] += 1
 
 
-res = [0] * n
-for i in range(n - 1, -1, -1):
-    count[Arr[i]] -= 1
-    res[count[Arr[i]]] = Arr[i]
+    for i in range(k):
+        count[i] += count[i - 1]
+
+
+    res = [0] * n
+    for i in range(n - 1, -1, -1):
+        count[Arr[i]] -= 1
+        res[count[Arr[i]]] = Arr[i]
     
+    return res
 
-print(res)
+print(counting_sort(Arr, 55))
