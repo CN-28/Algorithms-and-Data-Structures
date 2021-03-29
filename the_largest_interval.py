85c4088c382b9f1fb5cfad0fc685bfe52170e5a8
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-03-29 15:20:03 +0200

    bug fix

--- the_largest_interval.py
+++ the_largest_interval.py
@@ -37,7 +37,7 @@ def solve(intervals):
         for j in range(len(temp)):
             if temp[j][0] >= intervals[i][0] and temp[j][1] <= intervals[i][1]:
                 cnt += 1
-            elif temp[j][0] > intervals[i][1]:
+            elif temp[j][1] > intervals[i][1]:
                 break
         if cnt > maxi[0]:
             maxi = cnt, i
