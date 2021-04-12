c880cf54d771e534f118064cac6945a1d1745ace
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-04-12 16:42:10 +0200

    print all longest increasing subsequences

--- Dynamic programming/all_longest_increasing_sub.py
+++ Dynamic programming/all_longest_increasing_sub.py
@@ -1,34 +1,37 @@
-def lis(A):
+def printAllLIS(A):
     n = len(A)
+
     F = [1] * n
-    P = [[] for _ in range(n)]
-    omi = 0
-    omax = 0
     for i in range(1, n):
         for j in range(i):
             if A[j] < A[i] and F[j] + 1 > F[i]:
                 F[i] = F[j] + 1
-                P[i].append(j)
-        if F[i] > omax:
-            omax = F[i]
-            omi = i
         
-    print(P)
-    print(F)
-    print(omi, omax)
+            
+        
+    res = []
+    max_length = max(F)
+    for i in range(n):
+        if max_length == F[i]:
+            res.append((max_length, i, A[i], f"{A[i]} "))
+
+
+    cnt = 0
+    while len(res) > 0:
+        rem = res.pop()
+        if rem[0] == 1:
+            print(rem[3])
+            cnt += 1
+            
+
+        for i in range(rem[1] - 1, -1, -1):
+            if F[i] == rem[0] - 1 and A[i] <= rem[2]:
+                res.append((F[i], i, A[i], f"{A[i]} {rem[3]}"))
+        
+
+    return cnt
 
 
 
-    return (max(F), P)
-
-
-def printsolution(A, P, i):
-    if P[i] != -1:
-        printsolution(A, P, P[i])
-    print(A[i], end=" ")
-
-
-Arr = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
-print(Arr)
-x = lis(Arr)
-#printsolution(Arr, x[1], len(x[1]) - 1)
+A = [2,1,4,3]
+print(printAllLIS(A))
