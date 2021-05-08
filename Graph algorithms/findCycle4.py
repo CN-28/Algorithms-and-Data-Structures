73b63df4d13e8480dc6cf59e43e40da18a1623b8
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-05-08 21:51:21 +0200

    add find cycle of length 4

---
+++ Graph algorithms/findCycle4.py
@@ -0,0 +1,55 @@
+#find the cycle of length 4, adjacency matrix, time complexity: O(n**3)
+def findCycle4(G):
+    n = len(G)
+    for i in range(n):
+        for j in range(i + 1, n):
+            cnt = 0
+            for k in range(n):
+                if G[i][k] == G[j][k] == 1:
+                    cnt += 1
+            if cnt > 1:
+
+                cnt_2 = 0
+                for k in range(n):
+                    if G[i][k] == G[j][k] == 1:
+                        print(k, end=" ")
+                        cnt_2 += 1
+                        if cnt_2 == 2:
+                            break
+
+                print(i, j)
+                return True
+
+
+    return False
+
+
+
+G = [
+    [-1, 0, 0, 1, 1, 0],
+    [0, -1, 0, 1, 1, 1],
+    [0, 0, -1, 1, 1, 1],
+    [1, 1, 1, -1, 0, 0],
+    [1, 1, 1, 0, -1, 0],
+    [0, 1, 1, 0, 0, -1]
+]
+"""
+G = [
+    [-1, 1, 1, 1, 1, 1],
+    [1, -1, 1, 1, 1, 1],
+    [1, 1, -1, 1, 1, 1],
+    [1, 1, 1, -1, 1, 1],
+    [1, 1, 1, 1, -1, 1],
+    [1, 1, 1, 1, 1, -1]
+]
+"""
+"""
+G = [
+    [-1, 0, 0, 1, 0, 0],
+    [0, -1, 1, 1, 0, 0],
+    [0, 1, -1, 0, 0, 1],
+    [1, 1, 0, -1, 1, 0],
+    [0, 0, 1, 0, 1, -1]
+]
+"""
+print(findCycle4(G))
