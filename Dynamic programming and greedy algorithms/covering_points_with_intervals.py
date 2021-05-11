028d8a8205f2ee1e1f287936c748b87b3fc621ff
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-05-11 20:28:35 +0200

    add covering points with intervals

--- Dynamic programming and greedy algorithms/covering_points_with_intervals.py
+++ Dynamic programming and greedy algorithms/covering_points_with_intervals.py
@@ -2,3 +2,18 @@
 Given X - array of points, find the minimum number of intervals of size 1
 needed to cover all points from X
 """
+def solve(X):
+    n = len(X)
+    X.sort()
+    cnt = 1
+    curr = X[0]
+    for i in range(1, n):
+        if X[i] - curr >= 1:
+            cnt += 1
+            curr = X[i]
+
+    return cnt
+
+
+X = [2.7, 1.6, 1.7, 1.8, 1.9, 2.0, 2.8]
+print(solve(X))
