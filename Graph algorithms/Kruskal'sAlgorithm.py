a4d174a2f37c53948b38323ad2b7274908388048
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-05-23 19:33:51 +0200

    add Kruskal's algorithm

--- Graph algorithms/Kruskal'sAlgorithm.py
+++ Graph algorithms/Kruskal'sAlgorithm.py
@@ -16,30 +16,29 @@ def find(x):
 def union(x, y):
     x = find(x)
     y = find(y)
-    if x == y:
-        return False
-    if x.rank > y.rank:
-        y.parent = x
-    else:
-        x.parent = y
-        if x.rank == y.rank:
-            y.rank += 1
+    if x != y:
+        if x.rank > y.rank:
+            y.parent = x
+        else:
+            x.parent = y
+            if x.rank == y.rank:
+                y.rank += 1
+        return True
 
 
 
-def kruskal(G):
-    n = len(G)
+def kruskal(G, n):
+    V = [MakeSet(i) for i in range(n)]
     G.sort(key = lambda x: x[2])
-    print(G)
-    
 
     res = []
-    for i in range(n):
-        if union(MakeSet(G[i][0]), MakeSet(G[i][1])) == None:
-            res.append((G[i][0], G[i][1]))
+    for u, v, w in G:
+        if union(V[u], V[v]):
+            res.append((u, v))
     print(res)
-
     
-
+    
+    
 G = [(0, 1, 2), (0, 5, 7), (0, 3, 3), (0, 4, 8), (5, 4, 1), (1, 2, 5), (2, 4, 4), (2, 3, 6), (3, 4, 12)]
-kruskal(G)
+n = 6
+kruskal(G, n)
