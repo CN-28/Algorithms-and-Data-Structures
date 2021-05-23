993d52c45b7359ba5d383d069328fee993e4c526
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-05-23 15:04:37 +0200

    add Dijkstra's algorithm

---
+++ Graph algorithms/Dijkstra'sAlgorithm.py
@@ -0,0 +1,30 @@
+from queue import PriorityQueue
+import queue
+
+
+def dijkstra(G, V_s):
+    n = len(G)
+    visited = [False for _ in range(n)]
+    dist = [float('inf') for _ in range(n)]
+    path = [-1 for _ in range(n)]
+    dist[V_s] = 0
+
+
+    queue = PriorityQueue()
+    queue.put((0, V_s))
+    while not queue.empty():
+        c, u = queue.get()
+        visited[u] = True
+        for v, w in G[u]:
+            if not visited[v]:
+                if c + w < dist[v]:
+                    dist[v] = c + w
+                    path[v] = u
+                    queue.put((c + w, v))
+    
+    return path, dist
+
+
+
+G = [[(3, 1), (4, 2), (5, 3)], [(4, 2), (5, 3)], [(3, 5), (5, 4)], [(0, 1), (2, 5)], [(0, 2), (1, 2)], [(0, 3), (1, 3), (2, 4)]]
+print(dijkstra(G, 0))
