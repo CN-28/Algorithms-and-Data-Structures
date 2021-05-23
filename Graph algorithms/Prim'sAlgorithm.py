4f1491b0b1195d5ed1ea0345e9f70737b86d2f07
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-05-23 17:46:21 +0200

    add Prim's algorithm

--- Graph algorithms/Prim'sAlgorithm.py
+++ Graph algorithms/Prim'sAlgorithm.py
@@ -14,14 +14,14 @@ def prim(G):
         queue.put((G[0][i][1], 0, G[0][i][0]))
 
     while not queue.empty():
-        t = queue.get()
-        if not visited[t[2]]:
-            visited[t[2]] = True
-            mst[t[1]].append(t[2])
-            cost += t[0]
-            for i in range(len(G[t[2]])):
-                if not visited[G[t[2]][i][0]]:
-                    queue.put((G[t[2]][i][1], t[2], G[t[2]][i][0]))
+        c, prev, curr = queue.get()
+        if not visited[curr]:
+            visited[curr] = True
+            mst[prev].append(curr)
+            cost += c
+            for v, w in G[curr]:
+                if not visited[v]:
+                    queue.put((w, curr, v))
 
     return mst, cost
 
