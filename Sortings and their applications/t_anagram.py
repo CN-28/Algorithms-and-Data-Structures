0314bfb17581883a472060a133f9ca6297f30102
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-06-24 12:02:41 +0200

    add t_anagram

---
+++ Sortings and their applications/t_anagram.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+def tanagram(x, y, t):
+    n = len(x)
+    positions = [deque() for _ in range(26)]
+    for i in range(n):
+        positions[ord(y[i]) - 97].append(i)
+    for i in range(n):
+        closest = positions[ord(x[i]) - 97].popleft()
+        if abs(closest - i) > t:
+            return False
+
+
+    return True
