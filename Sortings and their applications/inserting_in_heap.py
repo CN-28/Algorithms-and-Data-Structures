34b960363b0d9b12ee2e6f44d546ed303a59a093
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-08-05 17:09:50 +0200

    add insertin in heap

--- Sortings and their applications/inserting_in_heap.py
+++ Sortings and their applications/inserting_in_heap.py
@@ -1,14 +1,33 @@
-def heapify(A, i):
-    parent = (i - 1)//2
-    if parent >= 0 and A[i] > A[parent]:
-        A[i], A[parent] = A[parent], A[i]
-        heapify(A, parent)
+def left(i):
+    return 2*i + 1
+
+def right(i):
+    return 2*i + 2
+
+def parent(i):
+    return (i - 1)//2
 
 
+def heapify(A, n, i):
+    l = left(i)
+    r = right(i)
+    m = i
+    if l < n and A[l] > A[m]:
+        m = l
+    if r < n and A[r] > A[m]:
+        m = r
+    
+    if m != i:
+        A[i], A[m] = A[m], A[i]
+        heapify(A, n, m)
 
-def insert(A, elem):
-    A.append(elem)
-    heapify(A, len(A) - 1)
+
+def insert(A, x):
+    A.append(x)
+    i = len(A) - 1
+    while i > 0 and A[parent(i)] < A[i]:
+        A[i], A[parent(i)] = A[parent(i)], A[i]
+        i = parent(i)
 
 tab = [5, 4, 2]
 
