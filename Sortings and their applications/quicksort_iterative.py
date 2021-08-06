259428b7f660b9cfb8814164a5273d00cdf19f80
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-08-06 15:40:18 +0200

    add iterative quicksort

--- Sortings and their applications/quicksort_iterative.py
+++ Sortings and their applications/quicksort_iterative.py
@@ -1,46 +1,30 @@
-def partition(Arr, p, r):
-    pivot = Arr[r]
-    i = p - 1
-    for j in range(p, r):
-        if Arr[j] <= pivot:
-            i += 1
-            Arr[i], Arr[j] = Arr[j], Arr[i]
+def quicksort(A, l, r):
+    stack = []
+    stack.append((l, r))
+    while stack:
+        l, r = stack.pop()
+        while l < r:
+            q = partition(A, l, r)
+            if q - l > r - q:
+                stack.append((l, q - 1))
+                l = q + 1
+            else:
+                stack.append((q + 1, r))
+                r = q - 1
 
-    Arr[i + 1], Arr[r], = Arr[r], Arr[i + 1]
+
+
+def partition(A, l, r):
+    i = l - 1
+    for j in range(l, r):
+        if A[j] < A[r]:
+            i += 1
+            A[i], A[j] = A[j], A[i]
+    A[i + 1], A[r] = A[r], A[i + 1]
     return i + 1
 
 
-def quicksort(Arr, p, r):
-    size_of_stack = r - p + 1
-    stack = [0] * size_of_stack
 
-    top_of_stack = -1
-    top_of_stack += 1
-    stack[top_of_stack] = p
-    top_of_stack += 1
-    stack[top_of_stack] = r
-
-    while top_of_stack >= 0:
-        r = stack[top_of_stack]
-        top_of_stack -= 1
-        p = stack[top_of_stack]
-        top_of_stack -= 1
-
-        q = partition(Arr, p, r)
-
-        if q - 1 > p:
-            top_of_stack += 1
-            stack[top_of_stack] = p
-            top_of_stack += 1
-            stack[top_of_stack] = q - 1
-        
-        if q + 1 < r:
-            top_of_stack += 1
-            stack[top_of_stack] = q + 1
-            top_of_stack += 1
-            stack[top_of_stack] = r
-
-
-Arr = [4, 1, 5, 7, 3, 4, 3]
-quicksort(Arr, 0, len(Arr) - 1)
-print(Arr)
+A = [3, 2, 1, 5, 4]
+quicksort(A, 0, len(A) - 1)
+print(A)
