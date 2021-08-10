38fa0ccd91e7dc5dfb44c508409e66319fff4ab5
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-08-10 18:40:51 +0200

    add magic fives algorithm - linear selection

--- Sortings and their applications/magic_fives_selection.py
+++ Sortings and their applications/magic_fives_selection.py
@@ -1,60 +1,67 @@
 #finding k-th (counting from 0 - first element) largest element in array in linear time,
 #assuming all pairs of numbers are different
+def linearselect(A, l, r, k):
+    while True:
+        if l == r:
+            return l
 
-def linearselect( A, k ):
-    def select(Arr, left, right, k):
-        if left == right:
-            return left
-        
-        pivot_index = select_pivot(Arr, left, right)
-        pivot_index = partition(Arr, left, right, pivot_index, k)
-
-        if k == pivot_index:
+        pivotInd = pivot(A, l, r)
+        pivotInd = partition(A, l, r, pivotInd)
+        if k == pivotInd:
             return k
-        elif k < pivot_index:
-            return select(Arr, left, pivot_index - 1, k)
+        elif k < pivotInd:
+            r = pivotInd - 1
         else:
-            return select(Arr, pivot_index + 1, right, k)
+            l = pivotInd + 1
+
+
+
+def partition(A, l, r, pivotInd):
+    pivot = A[pivotInd]
+    i = l - 1
+    j = r + 1
+    while True:
+        i += 1
+        while A[i] < pivot:
+            i += 1
+        
+        j -= 1
+        while A[j] > pivot:
+            j -= 1
+        
+        if i >= j:
+            return j
+        
+        A[i], A[j] = A[j], A[i]
+
+
+
+def pivot(A, l, r):
+    if r - l < 5:
+        return partition5(A, l, r)
     
-
-    def partition(Arr, left, right, pivot_index, k):
-        pivot = Arr[pivot_index]
-        Arr[pivot_index], Arr[right] = Arr[right], Arr[pivot_index]
-        i = left - 1
-        for j in range(left, right):
-            if Arr[j] <= pivot:
-                i += 1
-                Arr[i], Arr[j] = Arr[j], Arr[i]
-        Arr[i + 1], Arr[right] = Arr[right], Arr[i + 1]
-        
-        return i + 1
+    for i in range(l, r + 1, 5):
+        right5 = min(i + 4, r)
+        median5 = partition5(A, i, right5)
+        A[median5], A[l + (i - l)//5] = A[l + (i - l)//5], A[median5]
+    
+    mid = (r - l) // 10 + l + 1
+    return linearselect(A, l, l + (r - l)//5, mid)
 
 
-    def select_pivot(Arr, left, right):
-        if right - left + 1 <= 5:
-            return medianOf5(Arr, left, right)
-        
-        for i in range(left, right + 1, 5):
-            median_of_group = medianOf5(Arr, i, min(i + 4, right))
-            Arr[median_of_group], Arr[left + (i - left)//5] = Arr[left + (i - left)//5], Arr[median_of_group]
-        
-        mid = (right - left)//10 + left + 1
-        return select(Arr, left, left + (right - left)//5, mid)
-            
 
-    def medianOf5(Arr, left, right):
-        for i in range(left + 1, right + 1):
-            j = i
-            while j > left and Arr[j - 1] > Arr[j]:
-                Arr[j - 1], Arr[j] = Arr[j], Arr[j - 1]
-                j -= 1
-        
-        return (left + right)//2
+def partition5(A, l, r):
+    i = l + 1
+    while i <= r:
+        for j in range(i - 1, l - 1, -1):
+            if A[j] > A[i]:
+                A[i], A[j] = A[j], A[i]
+                break
+        i += 1
+    
+    return (l + r)//2
 
 
-    x = select(A, 0, len(A) - 1, k)
-    return A[x]
 
-
-Arr = [5, 3, 6, 2, 4]
-print(linearselect(Arr, 4))
+A = [90, 45, 25, 432, 123, 43, 15]
+print(A[linearselect(A, 0, len(A) - 1, 6)])
