bc02c4d2911a1a72da348335c9ce2b46275edad9
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-08-11 21:21:31 +0200

    add isAnagram, time complexities: O(n + k), O(n)

--- Sortings and their applications/isAnagram.py
+++ Sortings and their applications/isAnagram.py
@@ -27,27 +27,30 @@ B = 'aacgc'
 print(isAnagram1(A, B))
 
 
+
 #performance: O(n)
 def isAnagram2(A, B):
-    if len(A) != len(B):
+    n = len(A)
+    if n != len(B):
         return False
     
-    mini, maxi = float('inf'), 0
-    for i in range(len(A)):
-        maxi = max(ord(A[i]), ord(B[i]), maxi)
-        mini = min(ord(A[i]), ord(B[i]), mini)
+    stack = 0
+    count = [0 for _ in range(26)]
+    for i in range(n):
+        if count[ord(A[i]) - ord("a")] == 0:
+            stack += 1
+        count[ord(A[i]) - ord("a")] += 1
 
-    count = [0] * (maxi - mini + 1)
+    for i in range(n):
+        count[ord(B[i]) - ord("a")] -= 1
+        if count[ord(B[i]) - ord("a")] == 0:
+            stack -= 1
+        
+    if stack == 0:
+        return True
+    return False
 
-    for i in range(len(A)):
-        count[ord(A[i]) - mini] += 1
-        count[ord(B[i]) - mini] -= 1
-    
-    for i in range(len(A)):
-        if count[ord(A[i]) - mini] != 0:
-            return False
-    
-    return True
+
 
 A = 'accga'
 B = 'aacgc'
