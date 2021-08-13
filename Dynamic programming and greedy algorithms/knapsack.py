bb630cbe194031b366671ead8ce15a1fdef64b08
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-08-13 14:42:41 +0200

    add knapsack

--- Dynamic programming and greedy algorithms/knapsack.py
+++ Dynamic programming and greedy algorithms/knapsack.py
@@ -1,17 +1,21 @@
 def knapsack(W, P, MaxW):
     n = len(W)
-    F = [[0]*(MaxW + 1) for i in range(n)]
+    F = [[0 for _ in range(MaxW + 1)] for _ in range(n)]
 
     for w in range(W[0], MaxW + 1):
         F[0][w] = P[0]
     
+
     for i in range(1, n):
         for w in range(1, MaxW + 1):
-            F[i][w] = F[i - 1][w]
-            if w >= W[i]:
-                F[i][w] = max(F[i][w], F[i - 1][w - W[i]] + P[i])
+            if w < W[i]:
+                F[i][w] = F[i - 1][w]
+            else:
+                F[i][w] = max(F[i - 1][w], F[i - 1][w - W[i]] + P[i])
+ 
+ 
+    return F[n - 1][MaxW], F
 
-    return F[n-1][MaxW], F
 
 
 def getSolution(F, W, P, i, w):
@@ -19,12 +23,14 @@ def getSolution(F, W, P, i, w):
         if w >= W[0]:
             return [0]
         return []
-    
+
     if w >= W[i] and F[i][w] == F[i - 1][w - W[i]] + P[i]:
         return getSolution(F, W, P, i - 1, w - W[i]) + [i]
     return getSolution(F, W, P, i - 1, w)
 
+
+
 P = [10, 8, 4, 5, 3, 7]
 W = [4, 5, 12, 9, 1, 13]
-print(knapsack(W, P, 15)[0])
-print(getSolution(knapsack(W, P, 15)[1], W, P, len(P) - 1, 15))
+res, F = knapsack(W, P, 15)
+print(res, getSolution(F, W, P, len(W) - 1, 15))
