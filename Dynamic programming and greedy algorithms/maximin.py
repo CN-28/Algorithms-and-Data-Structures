50c1ac0b2499ad79d74fd3935f6f93375a8e7299
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-08-14 02:32:50 +0200

    add maximin

--- Dynamic programming and greedy algorithms/maximin.py
+++ Dynamic programming and greedy algorithms/maximin.py
@@ -1,47 +1,48 @@
-def maximin(Arr, k):
-    n = len(Arr)
-    sums = [0 for i in range(n)]
-    sums[0] = Arr[0]
-    for i in range(1, n):
-        sums[i] = Arr[i] + sums[i - 1]
-    
-
-    F = [[0 for j in range(k)] for i in range(n)]
-    S = [[0 for j in range(k)] for i in range(n)]
-    for i in range(n):
+"""
+Divide an array into k subarrays, such that the minimum sum from all subarrays is maximized
+"""
+def maximin(A, k):
+    n = len(A)
+    sums = [0 for i in range(n + 1)]
+    F = [[0 for _ in range(k)] for _ in range(n + 1)]
+    S = [[-1 for _ in range(k)] for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        sums[i] += sums[i - 1] + A[i - 1]
+ 
+    for i in range(n + 1):
         F[i][0] = sums[i]
-    
 
-    maxi = 0
     index = -1
-    for j in range(1, k):
-        for i in range(j, n):
-            for l in range(j - 1, i):
-                if min(F[l][j - 1], sums[i] - sums[l]) > maxi:
-                    maxi = min(F[l][j - 1], sums[i] - sums[l])
-                    index = l
-            S[i][j] = index
-            F[i][j] = maxi
+    for i in range(2, n + 1):
+        for t in range(1, min(i, k)):
             maxi = 0
-
+            for j in range(i, 0, -1):
+                mini = min(F[i - j][t - 1], sums[i] - sums[i - j])
+                if mini > maxi:
+                    maxi = mini
+                    index = i - j
+            F[i][t] = maxi
+            S[i][t] = index
     
-    temp = [-1 for i in range(k)]
-    index = n - 1
-    for i in range(k - 1, -1, -1):
-        temp[i] = S[index][i]
-        index = S[index][i]
     
-    print(Arr[:temp[1] + 1], end=" ")
-    for i in range(2, k):
-        print(Arr[temp[i - 1] + 1: temp[i] + 1], end=" ")
-    print(Arr[F[n - 1][k - 1] + 1:])
+    res = []
+    index = S[n][k - 1]
+    cnt = k - 1
+    end = n
+    while True:
+        res.append(A[index: end])
+        cnt -= 1
+        if cnt == -1:
+            break
+        end = index
+        index = S[index][cnt]
+        if index == -1:
+            index = 0 
 
 
-    return F[n - 1][k - 1]
+    return res[::-1], F[n][k - 1]
 
 
 
-Arr = [5, 3, 2, 6, 7, 4, 9, 6]
-print(Arr, end="\n")
-k = 5
-print(maximin(Arr, k))
+A = [5, 1, 3, 9, 15, 2, 4, 7, 0]
+print(maximin(A, 4))
