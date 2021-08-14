a50695bc8b2383f70e522cd3cc90907f8ea9be2a
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-08-14 20:25:40 +0200

    add black forest task

--- Dynamic programming and greedy algorithms/black_forest.py
+++ Dynamic programming and greedy algorithms/black_forest.py
@@ -2,48 +2,39 @@
 A[i] - profit for cutting an i-th tree, you mustn't cut two trees in a row,
 Find the maximum profit for cutting trees from array A
 """
-def solve(A):
+def maxCutProfit(A):
     n = len(A)
-    S = [-1 for i in range(n)]
+    F = [0 if i != 0 else A[i] for i in range(n)]
+    F[1] = max(A[0], A[1])
 
-    x2 = A[0]
-    if A[0] > A[1]:
-        x1 = A[0]
-        S[1] = 0
-    else:
-        x1 = A[1]
-
+    
     for i in range(2, n):
-        if x2 + A[i] > x1:
-            S[i] = i - 2
-            x1, x2 = x2 + A[i], x1
-        else:
-            x2 = x1
+        F[i] = max(F[i - 1], F[i - 2] + A[i])
     
 
+    i = n - 1
     res = []
-    index = n - 1
-    while index != 0 and index != 1:
-        if S[index] == -1:
-            index -= 1
+    while i >= 2:
+        if F[i] == F[i - 1]:
+            i -= 1
+        elif F[i] != F[i - 2]:
+            res.append(A[i])
+            i -= 2
+    
+    
+    if res[len(res) - 1] == 2:
+        res.append(A[0])
+    else:
+        if A[1] > A[0]:
+            res.append(A[1])
         else:
-            res.append(index)
-            index = S[index]
+            res.append(A[0])    
+    print(res[::-1])
     
 
-    if index == 1 and S[index] == 0:
-        res.append(0)
-    else:
-        res.append(1)
-    for i in range(len(res) - 1, -1, -1):
-        print(res[i], end="--> ")
-    print()
+    return F[n - 1]
 
 
 
-    return x1
-
-
-
-A = [3, 5, 9, 1000, 7, 2, 6, 1, 15]
-print(solve(A))
+forest = [5, 12, 6, 3, 2, 52, 11, 5, 34, 54]
+print(maxCutProfit(forest))
