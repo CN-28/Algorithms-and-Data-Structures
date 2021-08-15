94836a1459e27a6d058a446666fc139df3c99d26
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-08-16 00:37:40 +0200

    add hungry frog

--- Dynamic programming and greedy algorithms/hungry_frog.py
+++ Dynamic programming and greedy algorithms/hungry_frog.py
@@ -1,25 +1,26 @@
 """
 Find the minimum number of frog jumps to get from index 0 to n - 1, with given
-array A with energy potions on each index, to get to j-th index from i-th frog need to consume
-j - i energy points, frog starts with 0 energy points, and energy points mustn't drop below 0
+array A with energy potions on each index, to get to i-th index from j-th frog need to consume
+i - j energy points, frog starts with 0 energy points, and energy points mustn't drop below 0
 """
-def solve(A):
+def jump(A):
     n = len(A)
     F = [[float("inf") for _ in range(n)] for _ in range(n)]
     F[0][0] = 0
-    
 
     for i in range(1, n):
-        for j in range(n):
-            for k in range(i):
-                if j + A[k] - (i - k) >= 0:
-                    F[i][min(j + A[k] - (i - k), n - 1)] = min(F[i][min(j + A[k] - (i - k), n - 1)], F[k][j] + 1)
-        
+        for j in range(i):
+            for k in range(n):
+                if k - (i - j) + A[j] >= 0:
+                    F[i][min(k - (i - j) + A[j], n - 1)] = min(F[i][min(k - (i - j) + A[j], n - 1)], F[j][k] + 1)
     
     
-    return min(F[n - 1])
+    res = min(F[n - 1])
+    if res == float("inf"):
+        return False
+    return res
 
 
 
 A = [2, 2, 1, 0, 0, 0]
-print(solve(A))
+print(jump(A))
