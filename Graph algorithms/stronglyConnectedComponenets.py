6b01f6b97f84bde4a0103ba34d05c5794c6036fb
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-08-20 16:48:50 +0200

    add strongly connected components

--- Graph algorithms/stronglyConnectedComponenets.py
+++ Graph algorithms/stronglyConnectedComponenets.py
@@ -1,53 +1,48 @@
 #finding strongly connected components in directed graph
 def SCC(G):
     n = len(G)
+    
+    processingTime = []
     visited = [False for _ in range(n)]
-    timeArr = [-1 for _ in range(n)]
-    time = 0
-    def DFSVisit(i):
-        nonlocal time
+    def getTimes(i):
         visited[i] = True
         for j in range(n):
             if G[i][j] == 1 and not visited[j]:
-                DFSVisit(j)
-        
-        
-        timeArr[time] = i
-        time += 1
+                getTimes(j)
+
+        processingTime.append(i)
     
 
-    for i in range(n):
-        if not visited[i]:
-            DFSVisit(i)
+    getTimes(0)
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            if G[i][j] != G[j][i]:
+                G[i][j], G[j][i] = G[j][i], G[i][j]
+            
     
-    
-    for i in range(n):
-        for j in range(n):
-            if G[i][j] == 1:
-                G[i][j] = 0
-                G[j][i] = 2
-    
-    
-    def DFSVisit_comp(i):
+    def DFSVisit(i, temp):
         visited[i] = False
         for j in range(n):
+            if G[i][j] == 1 and visited[j]:
+                DFSVisit(j, temp)
 
-            if G[i][j] == 2 and visited[j]:
-                DFSVisit_comp(j)
-        
-        temp_arr.append(i)
-
-
-    SSC = []
-    temp_arr = []
-    for i in range(n - 1, -1, -1):
-        if visited[timeArr[i]]:
-            DFSVisit_comp(timeArr[i])
-            SSC.append(temp_arr)
-            temp_arr = []
+        temp.append(i)
     
-    return SSC
 
+    SCC = []
+    for i in range(n - 1, -1, -1):
+        temp = []
+        if visited[processingTime[i]]:
+            DFSVisit(processingTime[i], temp)
+            SCC.append(temp)
+    
+    #reconstruct the initial graph
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            if G[i][j] != G[j][i]:
+                G[i][j], G[j][i] = G[j][i], G[i][j]
+
+    return SCC
 
 
 
