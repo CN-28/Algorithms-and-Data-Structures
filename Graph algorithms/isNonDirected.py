2f4ff2727b44eb595782844d1a0bd3bb9d3f7bb1
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-08-20 03:47:18 +0200

    add isNonDirected

--- Graph algorithms/isNonDirected.py
+++ Graph algorithms/isNonDirected.py
@@ -1,17 +1,22 @@
+#time complexity: O(V + E)
 def isNonDirected(G):
     n = len(G)
-    A = [[0 for _ in range(n)] for _ in range(n)]
-    for i in range(n):
-        for j in range(len(G[i])):
-            A[i][G[i][j]] = 1
+    cnt = 0
+    for u in range(n):
+        for v in G[u]:
+            if u < v:
+                cnt += 1
+            else:
+                cnt -= 1
     
+    if cnt != 0:
+        return True
+    return False
     
-    for i in range(n):
-        for j in range(i, n):
-            if A[i][j] != A[j][i]:
-                return False
-    return True
 
 
+#directed
 G = [[3, 4, 5], [4, 5], [3, 5], [0, 2], [0, 1], [0, 1, 2]]
+#nondirected
+G = [[1, 2], [], [3], []]
 print(isNonDirected(G))
