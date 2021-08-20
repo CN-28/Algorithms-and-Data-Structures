aec9a35a24099516331f78bab2cbb6e9280d1bbf
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-08-20 23:34:38 +0200

    add select buildings

--- Dynamic programming and greedy algorithms/select_buildings.py
+++ Dynamic programming and greedy algorithms/select_buildings.py
@@ -1,41 +1,41 @@
 def prev(T, i):
-    a = 0
-    b = len(T) - 1
-    while a <= b:
-        mid = (a + b)//2
-        if T[mid][2] < T[i][1] and T[mid + 1][2] >= T[i][1] and mid + 1 < len(T):
-            return mid
-        if T[mid][2] < T[i][1]:
-            a = mid + 1
-        elif T[mid][2] >= T[i][1]:
-            b = mid - 1
-    
-    if T[mid][2] >= T[i][1]:
-        return -1
-    
-    return mid
+    left = 0
+    right = i - 1
+    mid = None
+    while left <= right:
+        mid = (left + right) // 2
+        if T[mid][2] >= T[i][1]:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return left - 1
+
 
 
 def select_buildings(T, p):
     n = len(T)
-    for i in range(n):
-        T[i] = T[i][0], T[i][1], T[i][2], T[i][3], i
+    T = [(T[i][0], T[i][1], T[i][2], T[i][3], i) for i in range(n)]
     T.sort(key = lambda x: x[2])
+
+
     F = [[0 for _ in range(p)] for _ in range(n)]
     for j in range(p):
         if j - T[0][3] >= 0:
             F[0][j] = T[0][0]*(T[0][2] - T[0][1])
+
+
     prevArr = [-1 for _ in range(n)]
     for i in range(1, n):
         prevArr[i] = prev(T, i)
 
 
-
     for i in range(1, n):
         for j in range(p):
             F[i][j] = F[i - 1][j]
-            if j - T[i][3] >= 0 and prevArr[i] != -1:
-                F[i][j] = max(F[prevArr[i]][j - T[i][3]] + T[i][0]*(T[i][2] - T[i][1]), F[i][j])
+            h, a, b, w, _ = T[i]
+            if j - w >= 0 and prevArr[i] != -1:
+                F[i][j] = max(F[prevArr[i]][j - w] + h * (b - a), F[i][j])
     
     
     maxi_ind = 0
@@ -57,3 +57,12 @@ def select_buildings(T, p):
     
     
     return sorted(res)
+
+
+
+#test cases
+T, p = [(2, 1, 5, 3), (3, 7, 9, 2), (2, 8, 11, 1)], 6
+T, p = [(8, 2, 6, 2), (9, 4, 8, 5), (9, 8, 9, 2), (3, 10, 15, 1)], 7
+T, p = [(7, 23, 24, 1), (2, 10, 14, 3), (7, 17, 22, 1), (9, 20, 22, 2), (4, 19, 22, 8), (2, 2, 6, 1)], 10
+T, p = [(1, 8, 12, 5), (4, 7, 8, 2), (3, 2, 3, 6), (9, 7, 8, 5), (8, 21, 22, 8), (5, 4, 7, 10), (1, 21, 24, 10), (7, 14, 16, 1)], 32
+print(select_buildings(T, p))
