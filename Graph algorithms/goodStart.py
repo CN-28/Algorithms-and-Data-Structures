bca88ce1b0413b406da8f89b52abfd994712b056
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-08-22 16:21:27 +0200

    add good start

--- Graph algorithms/goodStart.py
+++ Graph algorithms/goodStart.py
@@ -3,43 +3,36 @@
 def DFS(G):
     n = len(G)
     visited = [False for _ in range(n)]
-    time = 0
     v_good = -1
-    def DFSVisit(G, i):
-        nonlocal time, v_good
-        visited[i] = True
-        for v in G[i]:
-            if not visited[v]:
-                DFSVisit(G, v)
-
-        time += 1
-        if time == n:
-            v_good = i
-
     for i in range(n):
         if not visited[i]:
-            DFSVisit(G, i)
+            v_good = i
+            DFSVisit(G, visited, i)
     
+
     return v_good
     
 
-def DFSOnce(G, visited, i):
+
+def DFSVisit(G, visited, i):
     visited[i] = True
     for v in G[i]:
         if not visited[v]:
-            DFSOnce(G, visited, v)
+            DFSVisit(G, visited, v)
 
 
-G = [[1], [2, 3], [], [4], []]
 
-v_good = DFS(G)
-visited = [False for _ in range(len(G))]
-DFSOnce(G, visited, v_good)
-temp = 1
-for check in visited:
-    if not check:
-        print(False)
-        temp = 0
-        break
-if temp:
-    print(True)
+def goodStart(G):  
+    v_good = DFS(G)
+    visited = [False for _ in range(len(G))]
+    DFSVisit(G, visited, v_good)
+    for check in visited:
+        if not check:
+            return False
+    
+    return True, v_good
+
+
+
+G = [[], [0], [1, 3, 4], [], []]
+print(goodStart(G))
