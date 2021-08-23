51fe3df487f1ac3aa103b8919a920f0b99d41b2c
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-08-23 13:53:18 +0200

    add tourist guide problem

--- Graph algorithms/touristGuideProblem.py
+++ Graph algorithms/touristGuideProblem.py
@@ -1,49 +1,52 @@
 """
 The tourist guide wants to transport K tourists from the town A to the town B. There are many other towns along the way
 and buses with different capacities c_i run between the different towns. Find the minimum number of groups into which the guide must divide
-his group in order for everyone to get from A to B.
+his group in order for everyone to get from A to B, such that the group don't need to divide into smaller ones during the trip.
 """
 from queue import PriorityQueue
 def TGP(G, A, B, K):
-    n = len(G)
-    dist = [0 for _ in range(n)]
+    n = 0
+    for u, v, cost in G:
+        n = max(n, u + 1, v + 1)
+
+    GG = [[] for _ in range(n)]
+    for u, v, cost in G:
+        GG[u].append((v, cost))
+
+    
+    maxCap = [0 for _ in range(n)]
     parent = [-1 for _ in range(n)]
-
-
+    maxCap[A] = float("inf")
     Q = PriorityQueue()
     Q.put((-float("inf"), A))
     while not Q.empty():
         c, u = Q.get()
         c = -c
-        for v, w in G[u]:
-            if dist[v] < min(w, c):
-                dist[v] = min(w, c)
+        for v, w in GG[u]:
+            capacity = min(w, c)
+            if capacity > maxCap[v]:
+                maxCap[v] = capacity
                 parent[v] = u
-                Q.put((-dist[v], v))
-        
+                Q.put((-capacity, v))
+    
+    
     cnt = 0
-    if K % dist[v] == 0:
-        cnt = K//dist[v]
+    if K % maxCap[B] == 0:
+        cnt = K // maxCap[B]
     else:
-        cnt = K//dist[v] + 1
+        cnt = K // maxCap[B] + 1
 
-
+    
     path = []
-    path.append(B)
     temp = B
-    while temp != A:
-        temp = parent[temp]
+    while temp != -1:
         path.append(temp)
-    for i in range(len(path) - 1, -1, -1):
-        if i != 0:
-            print(path[i], end="---> ")
-        else:
-            print(path[i])
+        temp = parent[temp]
+    
 
-
-    return cnt
+    return cnt, path[::-1]
 
 
 
-G = [[(1, 25), (2, 15), (3, 17)], [(0, 25), (4, 22)], [(0, 15), (4, 20)], [(0, 17), (4, 35)], [(1, 22), (2, 20), (3, 35)]]
-print(TGP(G, 0, 3, 27))
+G = [(0, 2, 15), (2, 0, 15), (1, 0, 31), (0, 1, 31), (0, 3, 25), (3, 0, 25), (5, 2, 20), (2, 5, 20), (3, 5, 21), (5, 3, 21), (1, 4, 34), (4, 1, 34), (5, 4, 29), (4, 5, 29)]
+print(TGP(G, 0, 5, 58))
