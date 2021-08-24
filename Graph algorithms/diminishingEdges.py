b08c3c66a9d7a2ce5f25ed7dfc9bd4ca3991854e
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-08-24 19:32:01 +0200

    add diminishing edges

--- Graph algorithms/diminishingEdges.py
+++ Graph algorithms/diminishingEdges.py
@@ -1,42 +1,38 @@
 """
 In the graph G each edge has weight, check if there is a path from vertex v to vertex u
-which passes along edges with smaller and smaller weights
+which passes along edges with smaller and smaller weights.
+Weights of edges are from set {1, ... , |E|} (every pair of edges has different values) 
+time complexity: O(E)
 """
-def existsPath(G, Vs, Vf):
+def existsPath(G, x, y):
+    m = len(G)
+    bucketSort(G)
+    n = 0
+    for u, v, _ in G:
+        n = max(n, u + 1, v + 1)
+    
+
+    path = [False for _ in range(n)]
+    path[x] = True
+    for u, v, c in G:
+        path[v] |= path[u]
+        path[u] |= path[v]
+    
+    return path[y]
+
+
+
+def bucketSort(G):
     n = len(G)
-    visited = [False for _ in range(n)]
-    prev = float("inf")
-
-
-    visited[Vs] = True
-    def DFSVisit(i, prev):
-        visited[i] = True
-        for j in range(n):
-            if not visited[j] and G[i][j] > 0 and G[i][j] < prev:
-                if j == Vf:
-                    return True
-
-                prev = G[i][j]
-                if DFSVisit(j, prev):
-                    return True
-                
+    buckets = [[] for _ in range(n)]
 
     for i in range(n):
-        if not visited[i]:
-            if DFSVisit(i, prev):
-                return True
-            
-            
-    return False
+        buckets[G[i][2] - 1].append(G[i])
+
+    for i in range(n):
+        G[i] = buckets[n - 1 - i][0]
 
 
 
-G = [
-    [-1, 18, 16, 0, 0, 0],
-    [18, -1, 0, 17, 0, 0],
-    [16, 0, -1, 15, 0, 0],
-    [0, 17, 15, -1, 14, 0],
-    [0, 0, 0, 14, -1, 13],
-    [0, 0, 0, 0, 13, -1]
-]
-print(existsPath(G, 0, 5))
+G = [(0, 1, 1), (0, 2, 6), (1, 5, 2), (2, 5, 5), (4, 5, 3), (3, 5, 4)]
+print(existsPath(G, 0, 4))
